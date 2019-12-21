1db9b9796f3782484e265bd595dbeb92dbc07f06
Valerya <ivlikovalera.ph@gmail.com>
2019-12-21 18:07:31 +0700

    Делит код на компоненты

---
+++ src/components/movies-list-extra.js
@@ -0,0 +1,5 @@
+export const createMoviesListExtraTemplate = (listName) =>
+  `<section class="films-list--extra">
+  <h2 class="films-list__title">${listName}</h2>
+  <div class="films-list__container"></div>
+</section>`;
